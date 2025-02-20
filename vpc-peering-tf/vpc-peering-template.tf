ddb3e363aba3a72f015cc92b699445efd5f43f4d
Zack Goodger <99020440+Zack1667@users.noreply.github.com>
2025-02-20 14:18:16 +0000

    Update vpc-peering-template.tf

--- vpc-peering-tf/vpc-peering-template.tf
+++ vpc-peering-tf/vpc-peering-template.tf
@@ -1,111 +1,94 @@
 provider "aws" {
-  alias  = "account1"
-  region = "eu-west-1"
-  profile = "my-sso-profile" # Update with your AWS SSO profile name
+  alias   = "account1"
+  region  = "eu-west-1"
+  profile = "sso-profile" # Replace with correct AWS SSO profile
 }
 
 provider "aws" {
-  alias  = "account2"
-  region = "eu-west-1"
-  profile = "my-sso-profile" # Update with your AWS SSO profile name
+  alias   = "account2"
+  region  = "eu-west-1"
+  profile = "sso-profile" # Replace with correct AWS SSO profile
 }
 
-# Dev VPC in Account1
-module "dev_vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  providers = { aws = aws.account1 }
-  name    = "dev-vpc" # Update accordingly
-  cidr    = "10.200.241.0/24" # Update with your CIDR values
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  
-  tags = {
-    Environment = "dev" # Update accordingly
-  }
+# Variables - Update with actual values
+variable "dev_vpc_id" {
+  default = "vpc-xxxxxxxxxxxxxxxxx" # Replace with the Dev VPC ID
 }
 
-# Prod VPC in Account2
-module "prod_vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  providers = { aws = aws.account2 }
-  name    = "prod-vpc" # Update accordingly
-  cidr    = "10.222.2.0/24" # Update with your CIDR values
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  
-  tags = {
-    Environment = "prod" # Update accordingly
-  }
+variable "prod_vpc_id" {
+  default = "vpc-yyyyyyyyyyyyyyyyy" # Replace with the Prod VPC ID
 }
 
-# VPC Peering Connection
+variable "dev_private_route_table_id" {
+  default = "rtb-xxxxxxxxxxxxxxxxx" # Replace with Dev Private Route Table ID
+}
+
+variable "prod_private_route_table_id" {
+  default = "rtb-yyyyyyyyyyyyyyyyy" # Replace with Prod Private Route Table ID
+}
+
+variable "account2_id" {
+  default = "123456789012" # Replace with Account2 AWS Account ID
+}
+
+# VPC Peering Connection (Initiated from Account 1)
 resource "aws_vpc_peering_connection" "peer_dev_to_prod" {
-  provider   = aws.account1
-  vpc_id     = module.dev_vpc.vpc_id
-  peer_vpc_id = module.prod_vpc.vpc_id
-  peer_owner_id = "ACCOUNT2_ID" # Replace with Account2's AWS Account ID
-  auto_accept = false
+  provider       = aws.account1
+  vpc_id         = var.dev_vpc_id
+  peer_vpc_id    = var.prod_vpc_id
+  peer_owner_id  = var.account2_id
+  auto_accept    = false # Needs to be accepted by Account2
+
+  tags = {
+    Name = "dev-to-prod-peering"
+  }
 }
 
-# Accept Peering in Account2
+# Accept Peering in Account 2
 resource "aws_vpc_peering_connection_accepter" "accept_peer_dev_to_prod" {
-  provider   = aws.account2
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer_dev_to_prod.id
-  auto_accept = true
+  provider                   = aws.account2
+  vpc_peering_connection_id  = aws_vpc_peering_connection.peer_dev_to_prod.id
+  auto_accept                = true
+
+  tags = {
+    Name = "accept-dev-to-prod-peering"
+  }
 }
 
-# Route Table Updates in Account1 (Dev VPC)
+# Route Table Updates for Dev VPC (Account1)
 resource "aws_route" "dev_to_prod_route" {
-  provider = aws.account1
-  route_table_id         = "rtb-xxxx" # Replace with your route table id
-  destination_cidr_block = "10.222.2.0/24" # Update with your CIDR values
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer_dev_to_prod.id
+  provider                    = aws.account1
+  route_table_id              = var.dev_private_route_table_id
+  destination_cidr_block      = "10.222.2.0/24" # Update with Prod VPC CIDR
+  vpc_peering_connection_id   = aws_vpc_peering_connection.peer_dev_to_prod.id
 }
 
-# Route Table Updates in Account2 (Prod VPC)
+# Route Table Updates for Prod VPC (Account2)
 resource "aws_route" "prod_to_dev_route" {
-  provider = aws.account2
-  route_table_id         = "rtb-xxxx" # Replace with your route table id
-  destination_cidr_block = "10.200.241.0/24" # Update with your CIDR values
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer_dev_to_prod.id
+  provider                    = aws.account2
+  route_table_id              = var.prod_private_route_table_id
+  destination_cidr_block      = "10.200.241.0/24" # Update with Dev VPC CIDR
+  vpc_peering_connection_id   = aws_vpc_peering_connection.peer_dev_to_prod.id
 }
 
-# Security Group to Allow SSH and RDP in Dev VPC
-resource "aws_security_group" "dev_allow_ssh_rdp" {
-  provider = aws.account1
-  vpc_id   = module.dev_vpc.vpc_id
-  
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["10.222.2.0/24"] # Update with your CIDR values
-  }
-
-  ingress {
-    from_port   = 3389
-    to_port     = 3389
-    protocol    = "tcp"
-    cidr_blocks = ["10.222.2.0/24"] # Update with your CIDR values
-  }
+# Security Group Rules for Dev VPC (Allowing SSH/RDP from Prod VPC)
+resource "aws_security_group_rule" "dev_allow_ssh_rdp" {
+  provider                = aws.account1
+  security_group_id       = "sg-xxxxxxxxxxxxxxxxx" # Replace with Dev SG ID
+  type                    = "ingress"
+  from_port               = 22
+  to_port                 = 3389
+  protocol                = "tcp"
+  cidr_blocks             = ["10.222.2.0/24"] # Allow traffic from Prod VPC
 }
 
-# Security Group to Allow SSH and RDP in Prod VPC
-resource "aws_security_group" "prod_allow_ssh_rdp" {
-  provider = aws.account2
-  vpc_id   = module.prod_vpc.vpc_id
-  
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["10.200.241.0/24"] # Update with your CIDR values
-  }
-
-  ingress {
-    from_port   = 3389
-    to_port     = 3389
-    protocol    = "tcp"
-    cidr_blocks = ["10.200.241.0/24"] # Update with your CIDR values
-  } 
+# Security Group Rules for Prod VPC (Allowing SSH/RDP from Dev VPC)
+resource "aws_security_group_rule" "prod_allow_ssh_rdp" {
+  provider                = aws.account2
+  security_group_id       = "sg-yyyyyyyyyyyyyyyyy" # Replace with Prod SG ID
+  type                    = "ingress"
+  from_port               = 22
+  to_port                 = 3389
+  protocol                = "tcp"
+  cidr_blocks             = ["10.200.241.0/24"] # Allow traffic from Dev VPC
 }
